d8a94e4642d61bff47b0cdbd854b1bdfb24ab5ec
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-10-25 21:08:20 -0400

    change update-work-dto

--- src/works/dto/update-work.dto.ts
+++ src/works/dto/update-work.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateWorkDto } from './create-work.dto';
-import { IsEnum, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
 import { StatusProgress } from '@prisma/client';
 
 export class UpdateWorkDto extends PartialType(CreateWorkDto) {
@@ -12,7 +12,6 @@ export class UpdateWorkDto extends PartialType(CreateWorkDto) {
   @IsNotEmpty()
   userId: string;
 
-  @IsEnum(StatusProgress)
   @IsOptional()
   progress?: StatusProgress;
  
