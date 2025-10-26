8f85332b8eb03455174b2b21425f45ff3e9d27b1
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-10-25 21:02:05 -0400

    change in dto

--- src/clients/dto/update-client.dto.ts
+++ src/clients/dto/update-client.dto.ts
@@ -1,8 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
+import { Status } from '@prisma/client';
+import { Transform } from 'class-transformer';
 
 export class UpdateClientDto extends PartialType(CreateClientDto) {
-    
+    @Transform(({ value }) => value?.trimStart())
+    status: Status;
 }
 
 
--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -1,11 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCompanyDto } from './create-company.dto';
-import { IsEnum } from 'class-validator';
 import { Status } from '@prisma/client';
 import { Transform } from 'class-transformer';
 
 export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
-  @IsEnum(Status)
   @Transform(({ value }) => value?.trimStart())
   status: Status;
 }
