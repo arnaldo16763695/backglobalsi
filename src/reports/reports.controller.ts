1053959598c840ef252a027132f43ad919a03166
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-11-04 10:19:37 -0400

    create reports to clients and companies

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,18 +1,108 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { ReportsService } from './reports.service';
-import { Response } from 'express';
+import { Controller, Get, Res, Param } from "@nestjs/common";
+import { ReportsService } from "./reports.service";
+import { Response } from "express";
+import { Status } from "@prisma/client";
 
-@Controller('reports')
+@Controller("reports")
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
-  @Get('users')
-  async generateUsersReport( @Res() res: Response) {
+  @Get("users")
+  async generateUsersReport(@Res() res: Response) {
     const pdfDoc = await this.reportsService.generateUsersReport();
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename="users-report.pdf"');
-    pdfDoc.info.Title = 'Users Report';
-    pdfDoc.info.Author = 'GlobalSi';
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+
+  @Get("users/:filter")
+  async generateUsersReportByStatus(
+    @Res() res: Response,
+    @Param("filter") filter: Status
+  ) {
+    const pdfDoc = await this.reportsService.generateUsersReportByStatus(
+      filter
+    );
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+
+    @Get("clients")
+  async generateClientsReport(@Res() res: Response) {
+    const pdfDoc = await this.reportsService.generateClientsReport();
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+
+  @Get("clients/:filter")
+  async generateClientReportByStatus(
+    @Res() res: Response,
+    @Param("filter") filter: Status
+  ) {
+    const pdfDoc = await this.reportsService.generateClientsReportByStatus(
+      filter
+    );
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+
+  
+  @Get("companies")
+  async generateCompanyReport(@Res() res: Response) {
+    const pdfDoc = await this.reportsService.generateCompaniesReport();
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+
+  @Get("companies/:filter")
+  async generateCompanyReportByStatus(
+    @Res() res: Response,
+    @Param("filter") filter: Status
+  ) {
+    const pdfDoc = await this.reportsService.generateCompaniesReportByStatus(
+      filter
+    );
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="users-report.pdf"'
+    );
+    pdfDoc.info.Title = "Users Report";
+    pdfDoc.info.Author = "GlobalSi";
     pdfDoc.pipe(res);
     pdfDoc.end();
   }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,16 +1,74 @@
-import { Injectable } from '@nestjs/common';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
-import { PrinterService } from 'src/printer/printer.service';
-import { usersReport } from './documents/users.report';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
+import { PrinterService } from "src/printer/printer.service";
+import { usersReport } from "./documents/users.report";
+import { clientsReport } from "./documents/clients.report";
+import { PrismaService } from "src/prisma/prisma.service";
+import { Status } from '@prisma/client';
+import { companyReport } from "./documents/companies.report";
 
 @Injectable()
 export class ReportsService {
-constructor(private prisma: PrismaService, private readonly printer: PrinterService) {}
-
+  constructor(
+    private prisma: PrismaService,
+    private readonly printer: PrinterService
+  ) {}
+  //Users report 
   async generateUsersReport(): Promise<PDFKit.PDFDocument> {
     const users = await this.prisma.user.findMany();
-    const docDefinition:TDocumentDefinitions = usersReport(users);
+    const docDefinition: TDocumentDefinitions = usersReport(users);
+    return this.printer.createPdf(docDefinition);
+  }
+
+  async generateUsersReportByStatus(
+    filter: Status
+  ): Promise<PDFKit.PDFDocument> {
+    const users = await this.prisma.user.findMany({
+      where: {
+        status: filter,
+      },
+    });
+    const docDefinition: TDocumentDefinitions = usersReport(users);
+    return this.printer.createPdf(docDefinition);
+  }
+  
+  //Clients report 
+  
+  async generateClientsReport(): Promise<PDFKit.PDFDocument> {
+    const clients = await this.prisma.clients.findMany();
+    const docDefinition: TDocumentDefinitions = clientsReport(clients);
+    return this.printer.createPdf(docDefinition);
+  }
+
+  async generateClientsReportByStatus(
+    filter: Status
+  ): Promise<PDFKit.PDFDocument> {
+    const clients = await this.prisma.clients.findMany({
+      where: {
+        status: filter,
+      },
+    });
+    const docDefinition: TDocumentDefinitions = clientsReport(clients);
+    return this.printer.createPdf(docDefinition);
+  }
+
+  //Companies report 
+
+   async generateCompaniesReport(): Promise<PDFKit.PDFDocument> {
+    const companies = await this.prisma.company.findMany();
+    const docDefinition: TDocumentDefinitions = companyReport(companies);
+    return this.printer.createPdf(docDefinition);
+  }
+
+  async generateCompaniesReportByStatus(
+    filter: Status
+  ): Promise<PDFKit.PDFDocument> {
+    const companies = await this.prisma.company.findMany({
+      where: {
+        status: filter,
+      },
+    });
+    const docDefinition: TDocumentDefinitions = companyReport(companies);
     return this.printer.createPdf(docDefinition);
   }
 }
