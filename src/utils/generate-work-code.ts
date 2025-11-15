267fa7c2eb79f1bd103672016623781a8bf2341a
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-11-15 09:14:59 -0400

    chango function

--- src/utils/generate-work-code.ts
+++ src/utils/generate-work-code.ts
@@ -1,10 +1,10 @@
 
+import { getChileParts } from './helpers';
+
 export function generateOrderCode(sequence: number): string {
-    const now = new Date();
-    const year = now.getFullYear().toString().slice(-2); // '24'
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // '04'
-    const day = String(now.getDate()).padStart(2, '0'); // '12'
-    const seq = String(sequence).padStart(3, '0'); // '001'
-  
-    return `${year}${month}${day}${seq}`; // Ej: 240412001
-  }
+  const { year, month, day } = getChileParts();
+  const yy = year.slice(-2); // "25" de "2025"
+  const seq = String(sequence).padStart(3, '0');
+
+  return `${yy}${month}${day}${seq}`; // 251115001
+}
